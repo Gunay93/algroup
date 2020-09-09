335babe21ae9b5c0c3b9a8960958223d3edc301d
Gunay93 <h.gunay021093@gmail.com>
2020-09-09 14:44:19 +0400

    com1

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$('.carousel-product').owlCarousel({
+    loop:true,
+    autoplay: true,
+    margin:9,
+    items:3,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:5,
+        }
+    }
+});
+$('.icon-wishlist').click(function() {
+    $(this).find('svg path').toggleClass('fill-icon');
+});
